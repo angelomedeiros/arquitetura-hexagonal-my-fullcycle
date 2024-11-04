56e13b1815fd569256108ab9a22897c112529295
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-11-04 17:25:30 -0300

    test: creating TestProductService_Create

--- app/product_service_test.go
+++ app/product_service_test.go
@@ -24,3 +24,20 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, product)
 }
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mockapp.NewMockProductInterface(ctrl)
+	persistence := mockapp.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := app.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("product", 10)
+	require.Nil(t, err)
+	require.Equal(t, result, product)
+}
