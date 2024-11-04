576a52e15f56f8f202272834b812e8a4775a7fdd
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-11-04 17:20:45 -0300

    test: creating product_service_test.go

--- app/product_service_test.go
+++ app/product_service_test.go
@@ -1,1 +1,26 @@
 package app_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"hexagonal-aluno/app"
+	mockapp "hexagonal-aluno/app/mocks"
+	"testing"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mockapp.NewMockProductInterface(ctrl)
+	persistence := mockapp.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil)
+
+	service := app.ProductService{
+		Persistence: persistence,
+	}
+
+	_, err := service.Get("1")
+	require.Nil(t, err)
+	require.Equal(t, product, product)
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.9.0
+	go.uber.org/mock v0.5.0
 )
 
 require (
